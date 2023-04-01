9489e56c7cd75474d9407eb8d72b53bc885608e5
Ludovic Leger <ludlgr13@gmail.com>
2023-04-01 12:49:27 +0200

    Complete problem_2.js

--- problem_2.js
+++ problem_2.js
@@ -1,5 +1,20 @@
 function fiboEvenSum(n) {
-	for (let i = 1; i < n; i++) {
-		
-	return true;
+	let fiboSequence = [1]
+	sum = 0
+	
+	if (n > 1) {
+		fiboSequence.push(2)
+	}
+	
+	while (fiboSequence[fiboSequence.length - 1] < n) {
+		fiboSequence.push(fiboSequence[fiboSequence.length - 2] + fiboSequence[fiboSequence.length - 1])
+	}
+	
+	for (let num of fiboSequence) {
+		if (num % 2 == 0) {
+			sum += num
+		}
+	}
+	
+	return sum;
 }
