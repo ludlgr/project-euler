1d859639d65bbf58649afcda71e009899d398388
Ludovic Leger <ludlgr13@gmail.com>
2023-04-02 16:15:47 +0200

    Add problem_3.js

---
+++ problem_3.js
@@ -0,0 +1,7 @@
+/* Boucle qui va chercher à diviser number(puis son reste) par des nombres 
+premiers croissants jusqu'à ce qu'il reste 0 */
+
+function largestPrimeFactor(number) {
+
+  return true;
+}
